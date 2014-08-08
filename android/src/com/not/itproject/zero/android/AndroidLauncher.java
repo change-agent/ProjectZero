5bafad44c545500fda3aac72c446845e93fe1359
jyun.motion@gmail.com <jun@localhost.localdomain>
2014-08-08 12:26:54 +1000

    Create LibGDX project

---
+++ android/src/com/not/itproject/zero/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.not.itproject.zero.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.not.itproject.zero.ProjectZero;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ProjectZero(), config);
+	}
+}
